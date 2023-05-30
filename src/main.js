174fe10034bd6af7d3492aa48eab2378c3d6b961
TommasoPezzola <pezzola123@gmail.com>
2023-05-30 18:08:23 +0200

    add font awsome icons

--- src/main.js
+++ src/main.js
@@ -10,7 +10,11 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-library.add(faMagnifyingGlass, faBars)
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faMagnifyingGlass, faBars, faLinkedin, faFacebook, faSquareTwitter)
 
 
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
