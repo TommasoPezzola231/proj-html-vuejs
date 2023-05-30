89a560117b669156777304b77ef19d97fdc4a7b6
TommasoPezzola <pezzola123@gmail.com>
2023-05-30 17:39:13 +0200

    feat: add font-awesome

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import { createApp } from 'vue'
 import './styles/generic.scss'
+import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
-import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+library.add(faMagnifyingGlass, faBars)
+
 
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
-createApp(App).mount('#app')
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
