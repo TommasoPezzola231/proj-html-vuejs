edd94e12a8ecbd8c2009e8b6876e1fb3d7676d58
TommasoPezzola <pezzola123@gmail.com>
2023-05-30 17:15:24 +0200

    feat: add navbar

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,31 @@
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 import AppFooter from './components/AppFooter.vue'
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+  data() {
+    return {
+      navElements: [
+        "Homes",
+        "Pages",
+        "Blog",
+        "Shop",
+        "Events",
+        "Elements"
+      ]
+    }
+  }
+}
 </script>
 
 <template>
-  <AppHeader />
+  <AppHeader :navbar="navElements" />
 
   <AppMain />
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,40 @@
 <script>
 export default {
-    name: "AppHeader"
+    name: "AppHeader",
+    props: [
+        "navbar"
+    ]
 }
 </script>
 
-<template></template>
+<template>
+    <header>
 
-<style lang="scss" scoped></style>
+        <div class="customContainer d-flex justify-content-between align-items-center">
+            <div class="logo d-flex align-items-center">
+                <img src="../assets/logo-img-01.png">
+            </div>
+            <nav class="d-flex justify-content-around col-4">
+                <a v-for="element in navbar">{{ element }}</a>
+            </nav>
+        </div>
+
+        <!---------------                                                --------------->
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    div {
+        height: 100px;
+        background-color: #fdfdfd;
+    }
+
+}
+
+.logo {
+    img {
+        width: 160px;
+    }
+}
+</style>
