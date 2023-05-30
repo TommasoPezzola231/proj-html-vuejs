479ced013283bad93f80c805679cbec201023bba
TommasoPezzola <pezzola123@gmail.com>
2023-05-30 17:14:41 +0200

    feat: add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import './styles/generic.scss'
-import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
+
+import App from './App.vue'
+
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
-createApp(App).use(bootstrap).mount('#app')
+createApp(App).mount('#app')
